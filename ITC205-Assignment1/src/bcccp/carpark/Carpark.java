c52a6f6ea3f42982d4e021acc106ec03bad21f4f
Matthew <matthewjuliusscott@gmail.com>
2017-07-29 19:19:49 +1000

    Started in implementing entity classes, carpark entity interface and observer

--- ITC205-Assignment1/src/bcccp/carpark/Carpark.java
+++ ITC205-Assignment1/src/bcccp/carpark/Carpark.java
@@ -1,5 +1,6 @@
 package bcccp.carpark;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bcccp.tickets.adhoc.IAdhocTicket;
@@ -8,79 +9,106 @@ import bcccp.tickets.season.ISeasonTicket;
 import bcccp.tickets.season.ISeasonTicketDAO;
 
 public class Carpark implements ICarpark {
-	
+
 	private List<ICarparkObserver> observers;
 	private String carparkId;
 	private int capacity;
 	private int numberOfCarsParked;
 	private IAdhocTicketDAO adhocTicketDAO;
 	private ISeasonTicketDAO seasonTicketDAO;
-	
-	
-	
-	public Carpark(String name, int capacity, 
-			IAdhocTicketDAO adhocTicketDAO, 
-			ISeasonTicketDAO seasonTicketDAO) {
-		//TODO Implement constructor
+	private String name;
+	private boolean isFull;
+
+	/**
+	 * Instantiates a new carpark.
+	 *
+	 * @param name
+	 *            the name
+	 * @param capacity
+	 *            the capacity
+	 * @param adhocTicketDAO
+	 *            the adhoc ticket DAO
+	 * @param seasonTicketDAO
+	 *            the season ticket DAO
+	 */
+	public Carpark(String name, int capacity, IAdhocTicketDAO adhocTicketDAO, ISeasonTicketDAO seasonTicketDAO) {
+		this.name = name;
+		this.capacity = capacity;
+		this.adhocTicketDAO = adhocTicketDAO;
+		this.seasonTicketDAO = seasonTicketDAO;
 	}
 
-
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bcccp.carpark.ICarpark#register(bcccp.carpark.ICarparkObserver)
+	 */
 	@Override
 	public void register(ICarparkObserver observer) {
-		// TODO Auto-generated method stub
-		
+		if (observers == null) {
+			observers = new ArrayList<ICarparkObserver>();
+		}
+		observers.add(observer);
 	}
 
-
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bcccp.carpark.ICarpark#deregister(bcccp.carpark.ICarparkObserver)
+	 */
 	@Override
 	public void deregister(ICarparkObserver observer) {
-		// TODO Auto-generated method stub
-		
+		if (observers != null) {
+			observers.remove(observer);
+		}
 	}
 
-
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bcccp.carpark.ICarpark#getName()
+	 */
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
-
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bcccp.carpark.ICarpark#isFull()
+	 */
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		return isFull;
 	}
 
-
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see bcccp.carpark.ICarpark#issueAdhocTicket()
+	 */
 	@Override
 	public IAdhocTicket issueAdhocTicket() {
-		// TODO Auto-generated method stub
-		return null;
+		return adhocTicketDAO.createTicket(carparkId);
 	}
 
-
-
+	/* (non-Javadoc)
+	 * @see bcccp.carpark.ICarpark#recordAdhocTicketEntry()
+	 */
 	@Override
 	public void recordAdhocTicketEntry() {
 		// TODO Auto-generated method stub
-		
 	}
 
-
-
+	/* (non-Javadoc)
+	 * @see bcccp.carpark.ICarpark#getAdhocTicket(java.lang.String)
+	 */
 	@Override
 	public IAdhocTicket getAdhocTicket(String barcode) {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO check interface implementation, exceptions etc. what happens if ticket doesnt exist?
+		return adhocTicketDAO.findTicketByBarcode(barcode);
 	}
-
-
 
 	@Override
 	public float calculateAddHocTicketCharge(long entryDateTime) {
@@ -88,31 +116,23 @@ public class Carpark implements ICarpark {
 		return 0;
 	}
 
-
-
 	@Override
 	public void recordAdhocTicketExit() {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public void registerSeasonTicket(ISeasonTicket seasonTicket) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public void deregisterSeasonTicket(ISeasonTicket seasonTicket) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public boolean isSeasonTicketValid(String ticketId) {
@@ -120,31 +140,22 @@ public class Carpark implements ICarpark {
 		return false;
 	}
 
-
-
 	@Override
 	public boolean isSeasonTicketInUse(String ticketId) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-
-
 	@Override
 	public void recordSeasonTicketEntry(String ticketId) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public void recordSeasonTicketExit(String ticketId) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	
-	
+	}
 
 }
--- ITC205-Assignment1/src/bcccp/carpark/ICarparkObserver.java
+++ ITC205-Assignment1/src/bcccp/carpark/ICarparkObserver.java
@@ -1,7 +1,16 @@
 package bcccp.carpark;
 
+/**
+ * An asynchronous update interface for receiving notifications
+ * about ICarpark information as the ICarpark is constructed.
+ */
 public interface ICarparkObserver {
 	
+	/**
+	 * This method is called when information about an ICarpark
+	 * which was previously requested using an asynchronous
+	 * interface becomes available.
+	 */
 	public void notifyCarparkEvent();
 	
 
