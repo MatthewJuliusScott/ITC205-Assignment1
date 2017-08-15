86f84498598a531abf88ebd438cde3d4405392d7
Matthew <matthewjuliusscott@gmail.com>
2017-08-15 18:07:30 +1000

    committing James's changes

--- ITC205-Assignment1/src/bcccp/tickets/adhoc/AdhocTicket.java
+++ ITC205-Assignment1/src/bcccp/tickets/adhoc/AdhocTicket.java
@@ -2,162 +2,138 @@ package bcccp.tickets.adhoc;
 
 import java.util.Date;
 
-/**
- * The Class AdhocTicket.
- */
 public class AdhocTicket implements IAdhocTicket {
 	
-	/** The Carpark id. */
-	private String carparkId;
-	
-	/** The ticket no. */
-	private int ticketNo;
-	
-	/** The entry date time. */
+	private String carparkId_;
+	private int ticketNo_;
 	private long entryDateTime;
-	
-	/** The paid date time. */
 	private long paidDateTime;
-	
-	/** The exit date time. */
-	private long exitDateTime = 0l;
-	
-	/** The charge. */
+	private long exitDateTime;
 	private float charge;
-	
-	/** The barcode. */
 	private String barcode;
+	private STATE state_;
+	
+	private enum STATE { ISSUED, CURRENT, PAID, EXITED }
 
 	
 	
-	/**
-	 * Instantiates a new adhoc ticket.
-	 *
-	 * @param carparkId the carpark id
-	 * @param ticketNo the ticket no
-	 * @param barcode the barcode
-	 */
 	public AdhocTicket(String carparkId, int ticketNo, String barcode) {
-		this.carparkId = carparkId;
-		this.ticketNo = ticketNo;
+		this.carparkId_ = carparkId;
+		this.ticketNo_ = ticketNo;
 		this.barcode = barcode;
+		this.state_ = STATE.ISSUED;		
 	}
 
-
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#getTicketNo()
-	 */
-	@Override
-	public int getTicketNo() {
-		return ticketNo;
-	}
-
-
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#getBarcode()
-	 */
+	
+	
 	@Override
 	public String getBarcode() {
 		return barcode;
 	}
 
 
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#getCarparkId()
-	 */
+	
 	@Override
 	public String getCarparkId() {
-		return carparkId;
+		return carparkId_;
 	}
 
-
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#enter(long)
-	 */
+	
+	
 	@Override
-	public void enter(long dateTime) {
-		entryDateTime = dateTime;
+	public int getTicketNo() {
+		return ticketNo_;
 	}
+	
 
-
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#getEntryDateTime()
-	 */
+	
+	@Override
+	public void enter(long entryDateTime) {
+		this.entryDateTime = entryDateTime;
+		this.state_ = STATE.CURRENT;		
+	}
+	
+	
+	
 	@Override
 	public long getEntryDateTime() {
 		return entryDateTime;
 	}
 
-
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#isCurrent()
-	 */
+	
+	
 	@Override
-	public boolean isCurrent() {
-		long now = new Date().getTime();
-		return now >= getEntryDateTime() && now < getExitDateTime();
+	public void pay(long paidDateTime, float charge) {
+		this.paidDateTime = paidDateTime;
+		this.charge = charge;
+		state_ = STATE.PAID;
 	}
-
-
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#pay(long, float)
-	 */
-	@Override
-	public void pay(long dateTime, float charge) {
-		paidDateTime = dateTime;
-		this.charge -= charge;
-	}
-
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#getPaidDateTime()
-	 */
+	
+	
+	
 	@Override
 	public long getPaidDateTime() {
 		return paidDateTime;
 	}
 
 
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#isPaid()
-	 */
-	@Override
-	public boolean isPaid() {
-		return charge <= 0f;
-	}
 
-
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#getCharge()
-	 */
 	@Override
 	public float getCharge() {
 		return charge;
 	}
 
+	
+	
+	public String toString() {
+		Date entryDate = new Date(entryDateTime);
+		Date paidDate = new Date(paidDateTime);
+		Date exitDate = new Date(exitDateTime);
 
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#exit(long)
-	 */
-	@Override
-	public void exit(long dateTime) {
-		exitDateTime = dateTime;
+		return "Carpark    : " + carparkId_ + "\n" +
+		       "Ticket No  : " + ticketNo_ + "\n" +
+		       "Entry Time : " + entryDate + "\n" + 
+		       "Paid Time  : " + paidDate + "\n" + 
+		       "Exit Time  : " + exitDate + "\n" +
+		       "State      : " + state_ + "\n" +
+		       "Barcode    : " + barcode;		
 	}
 
 
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#getExitDateTime()
-	 */
+
+	@Override
+	public boolean isCurrent() {
+		return state_ == STATE.CURRENT;
+	}
+
+
+
+	@Override
+	public boolean isPaid() {
+		return state_ == STATE.PAID;
+	}
+
+
+
+	@Override
+	public void exit(long dateTime) {
+		exitDateTime = dateTime;
+		state_ = STATE.EXITED;
+	}
+
+
+
 	@Override
 	public long getExitDateTime() {
 		return exitDateTime;
 	}
 
 
-	/* (non-Javadoc)
-	 * @see bcccp.tickets.adhoc.IAdhocTicket#hasExited()
-	 */
+
 	@Override
 	public boolean hasExited() {
-		return  exitDateTime != 0l;
+		return state_ == STATE.EXITED;
 	}
+
+
 }
