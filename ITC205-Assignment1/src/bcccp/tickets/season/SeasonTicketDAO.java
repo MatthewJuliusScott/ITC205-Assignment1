ca6e4989376f935da89bb2519cd75d4fb0f4d3e6
Matthew <matthewjuliusscott@gmail.com>
2017-08-18 20:32:47 +1000

    removing auto-format prefs

--- ITC205-Assignment1/src/bcccp/tickets/season/SeasonTicketDAO.java
+++ ITC205-Assignment1/src/bcccp/tickets/season/SeasonTicketDAO.java
@@ -5,9 +5,7 @@ import java.util.HashMap;
 
 public class SeasonTicketDAO implements ISeasonTicketDAO {
 
-	private IUsageRecordFactory				factory;	// TODO Private class
-	                                                    // variables should have
-	                                                    // underscore suffix.
+	private IUsageRecordFactory				factory; //TODO Private class variables should have underscore suffix.
 
 	// need to keep track of all tickets
 	private HashMap<String, ISeasonTicket>	tickets;
