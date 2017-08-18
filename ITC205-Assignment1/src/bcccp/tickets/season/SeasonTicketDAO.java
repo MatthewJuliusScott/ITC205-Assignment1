fe385614e68114be6c65a08b41c5241e56a715c8
Matthew <matthewjuliusscott@gmail.com>
2017-08-18 20:33:46 +1000

    Generic variables should have the same name as their type.

--- ITC205-Assignment1/src/bcccp/tickets/season/SeasonTicketDAO.java
+++ ITC205-Assignment1/src/bcccp/tickets/season/SeasonTicketDAO.java
@@ -5,15 +5,10 @@ import java.util.HashMap;
 
 public class SeasonTicketDAO implements ISeasonTicketDAO {
 
-	private final IUsageRecordFactory				factory;	// TODO Private
-	                                                            // class
-	                                                            // variables
-	                                                            // should have
-	                                                            // underscore
-	                                                            // suffix.
+	private IUsageRecordFactory				factory; //TODO Private class variables should have underscore suffix.
 
 	// need to keep track of all tickets
-	private final HashMap<String, ISeasonTicket>	tickets;
+	private HashMap<String, ISeasonTicket>	tickets;
 
 	public SeasonTicketDAO(IUsageRecordFactory factory) {
 		this.factory = factory;
@@ -21,10 +16,7 @@ public class SeasonTicketDAO implements ISeasonTicketDAO {
 	}
 
 	@Override
-	public void deregisterTicket(ISeasonTicket ticket) { // TODO Generic
-	                                                     // variables should
-	                                                     // have the same name
-	                                                     // as their type.
+	public void deregisterTicket(ISeasonTicket ticket) { //TODO Generic variables should have the same name as their type.
 
 		tickets.remove(ticket.getId());
 	}
@@ -44,10 +36,10 @@ public class SeasonTicketDAO implements ISeasonTicketDAO {
 	@Override
 	public void recordTicketEntry(String ticketId) {
 
-		final ISeasonTicket newTicket = tickets.get(ticketId);
+		ISeasonTicket newTicket = tickets.get(ticketId);
 
-		final long time = System.currentTimeMillis();
-		final IUsageRecord usage = factory.make(ticketId, time);
+		long time = System.currentTimeMillis();
+		IUsageRecord usage = factory.make(ticketId, time);
 
 		newTicket.recordUsage(usage);
 	}
@@ -55,9 +47,9 @@ public class SeasonTicketDAO implements ISeasonTicketDAO {
 	@Override
 	public void recordTicketExit(String ticketId) {
 
-		final ISeasonTicket oldTicket = tickets.get(ticketId);
+		ISeasonTicket oldTicket = tickets.get(ticketId);
 
-		final long time = System.currentTimeMillis();
+		long time = System.currentTimeMillis();
 		oldTicket.getCurrentUsageRecord().finalise(time);
 
 	}
