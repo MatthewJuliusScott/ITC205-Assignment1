113b6b6fcb03849a80a722568e135b86763f56f6
barto_is_vin@hotmail.com <barto_is_vin@hotmail.com>
2017-08-19 17:37:59 +1000

    Rule 63 Interface declarations should be on next line

--- ITC205-Assignment1/src/bcccp/carpark/exit/ExitController.java
+++ ITC205-Assignment1/src/bcccp/carpark/exit/ExitController.java
@@ -10,10 +10,58 @@ import bcccp.carpark.ICarpark;
 import bcccp.carpark.IGate;
 import bcccp.tickets.adhoc.IAdhocTicket;
 
+<<<<<<< HEAD
 public class ExitController implements ICarSensorResponder, IExitController { //TODO Rule 63 Interface declarations should be on next line
+=======
+public class ExitController implements ICarSensorResponder, IExitController {
+>>>>>>> branch 'master' of https://github.com/MatthewJuliusScott/ITC205-Team-Repository.git
 
 	private IGate			exitGate;
+<<<<<<< HEAD
+=======
 
+	private ICarSensor		insideSensor;
+
+	private ICarSensor		outsideSensor;
+
+	private IExitUI			ui;
+
+	private ICarpark		carpark;
+
+	private IAdhocTicket	adhocTicket		= null;
+
+	private long			exitTime;
+
+	private String			seasonTicketId	= null;
+
+	/**
+	 * Instantiates a new exit controller.
+	 *
+	 * @param carpark
+	 *            the carpark
+	 * @param exitGate
+	 *            the exit gate
+	 * @param is
+	 *            the inside sensor
+	 * @param os
+	 *            the outside sensor
+	 * @param ui
+	 *            the user interface
+	 */
+	public ExitController(Carpark carpark, IGate exitGate, ICarSensor is,
+	        ICarSensor os, IExitUI ui) {
+		this.carpark = carpark;
+		this.exitGate = exitGate;
+		this.insideSensor = is;
+		this.insideSensor.registerResponder(this);
+		this.outsideSensor = os;
+		this.outsideSensor.registerResponder(this);
+		this.ui = ui;
+		this.ui.registerController(this);
+	}
+>>>>>>> branch 'master' of https://github.com/MatthewJuliusScott/ITC205-Team-Repository.git
+
+<<<<<<< HEAD
 	private ICarSensor		insideSensor;
 
 	private ICarSensor		outsideSensor;
@@ -60,10 +108,23 @@ public class ExitController implements ICarSensorResponder, IExitController { //
 	 * @see bcccp.carpark.ICarSensorResponder#carEventDetected(java.lang.String,
 	 * boolean)
 	 */  
+=======
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see bcccp.carpark.ICarSensorResponder#carEventDetected(java.lang.String,
+	 * boolean)
+	 */
+>>>>>>> branch 'master' of https://github.com/MatthewJuliusScott/ITC205-Team-Repository.git
 	@Override
 	public void carEventDetected(String detectorId, boolean detected) {
+<<<<<<< HEAD
 		if (detectorId == insideSensor.getId() && detected) { 
 			ui.display("Please insert ticket."); //TODO First parenthesis in methods can have a space before them (Rule 74).
+=======
+		if (detectorId == insideSensor.getId() && detected) {
+			ui.display("Please insert ticket.");
+>>>>>>> branch 'master' of https://github.com/MatthewJuliusScott/ITC205-Team-Repository.git
 		} else if (detectorId == outsideSensor.getId() && detected) {
 			exitGate.lower();
 			ui.display("");
