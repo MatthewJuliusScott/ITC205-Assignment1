33da7be036bc6d60de7df4341d8aed5de3fc86da
barto_is_vin@hotmail.com <barto_is_vin@hotmail.com>
2017-08-20 13:46:32 +1000

    Private class variables given underscore suffix

--- ITC205-Assignment1/src/bcccp/tickets/season/SeasonTicketDAO.java
+++ ITC205-Assignment1/src/bcccp/tickets/season/SeasonTicketDAO.java
@@ -5,41 +5,41 @@ import java.util.HashMap;
 
 public class SeasonTicketDAO implements ISeasonTicketDAO { // TODO Abbreviations in names should be avoided.
 
-	private IUsageRecordFactory				factory; //TODO Private class variables should have underscore suffix.
+	private IUsageRecordFactory				factory_; //TODO Private class variables should have underscore suffix.
 
 	// need to keep track of all tickets
-	private HashMap<String, ISeasonTicket>	tickets;
+	private HashMap<String, ISeasonTicket>	tickets_;
 
 	public SeasonTicketDAO(IUsageRecordFactory factory) {
-		this.factory = factory;
-		tickets = new HashMap<String, ISeasonTicket>();
+		this.factory_ = factory;
+		tickets_ = new HashMap<String, ISeasonTicket>();
 	}
 
 	@Override
 	public void deregisterTicket(ISeasonTicket ticket) { //TODO Generic variables should have the same name as their type.
 
-		tickets.remove(ticket.getId());
+		tickets_.remove(ticket.getId());
 	}
 
 	@Override
 	public ISeasonTicket findTicketById(String ticketId) {
 
-		return tickets.get(ticketId);
+		return tickets_.get(ticketId);
 	}
 
 	@Override
 	public int getNumberOfTickets() {
 
-		return tickets.size();
+		return tickets_.size();
 	}
 
 	@Override
 	public void recordTicketEntry(String ticketId) {
 
-		ISeasonTicket newTicket = tickets.get(ticketId);
+		ISeasonTicket newTicket = tickets_.get(ticketId);
 
 		long time = System.currentTimeMillis();
-		IUsageRecord usage = factory.make(ticketId, time);
+		IUsageRecord usage = factory_.newUsageRecord (ticketId, time);
 
 		newTicket.recordUsage(usage);
 	}
@@ -47,7 +47,7 @@ public class SeasonTicketDAO implements ISeasonTicketDAO { // TODO Abbreviations
 	@Override
 	public void recordTicketExit(String ticketId) {
 
-		ISeasonTicket oldTicket = tickets.get(ticketId);
+		ISeasonTicket oldTicket = tickets_.get(ticketId);
 
 		long time = System.currentTimeMillis();
 		oldTicket.getCurrentUsageRecord().finalise(time);
@@ -56,7 +56,7 @@ public class SeasonTicketDAO implements ISeasonTicketDAO { // TODO Abbreviations
 
 	@Override
 	public void registerTicket(ISeasonTicket ticket) {
-		tickets.put(ticket.getId(), ticket);
+		tickets_.put(ticket.getId(), ticket);
 	}
 
 }
