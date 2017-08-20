d0990978fe0c7a21ab7020c40d1c768d59f847ca
barto_is_vin@hotmail.com <barto_is_vin@hotmail.com>
2017-08-20 12:51:10 +1000

    Changed factory method to newUsageRecord to adhere to standards.

--- ITC205-Assignment1/src/bcccp/tickets/season/UsageRecordFactory.java
+++ ITC205-Assignment1/src/bcccp/tickets/season/UsageRecordFactory.java
@@ -5,7 +5,7 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 	@Override
 	public IUsageRecord newUsageRecord(String ticketId, long startDateTime) { //TODO Classes that creates instances on behalf of others (factories) can do so through method new[ClassName].
 		
-		return new UsageRecord(ticketId, startDateTime); // Is this really a factory ?
+		return new UsageRecord(ticketId, startDateTime); 
 	}
 
 
